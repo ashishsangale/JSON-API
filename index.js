dae3d09461df72f3f37e4733ec936ba9b2f9963b
Ashish Sangale <aashishsangale07@gmail.com>
2020-06-02 15:47:06 +0530

    bodyParser

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express = require('express');
     app = express();
     todos = require('./routes/todos');
-
+    db = require('./models');
+    bodyParser = require('body-parser');
+    
 app.use('/api/todos', todos)
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     res.send("TESTTTT")
 });
+
 
 app.listen(3000 || process.env.PORT, process.env.IP, function(){
     console.log('startingg')
