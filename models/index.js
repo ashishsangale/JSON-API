a731249a4c53e24162fb6050b1bfd4bdcfb0fe0c
Ashish Sangale <aashishsangale07@gmail.com>
2020-06-02 15:46:27 +0530

    models

--- models/index.js
+++ models/index.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 mongoose.set('debug', true)
 mongoose.Promise = Promise;
 
-mongoose.connect('mongodb://localhost/todo-api');
+mongoose.connect('mongodb://localhost/todo-api',{ useUnifiedTopology: true }, function(){
+    console.log('connected');
+});
 
 module.exports.Todo = require('./todo');
