566d41a50edf0847b238d4747f020a0bc50b37a8
Ashish Sangale <aashishsangale07@gmail.com>
2020-06-02 15:46:01 +0530

    routes

--- routes/todos.js
+++ routes/todos.js
@@ -1,13 +1,26 @@
 var express = require('express');
     app = express();
     router = express.Router();
-
+    bodyParser = require('body-parser');
 var db = require('../models');
 
+
+router.use(bodyParser.json());
 router.get('/', function(req, res){
     db.Todo.find()
     .then(function(todos){
         res.json(todos)
+    })
+    .catch(function(err){
+        res.send(err)
+    })
+});
+
+router.post('/', function(req, res){
+    console.log(req.body)
+    db.Todo.create(req.body)
+    .then(function(newTodo){
+        res.json(newTodo)
     })
     .catch(function(err){
         res.send(err)
